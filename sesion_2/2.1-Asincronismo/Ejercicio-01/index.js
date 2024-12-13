c77629a74004c9151138c598ace427e812d42894
Sebastian Luna <selunar@outlook.com>
2024-12-13 01:43:26 -0500

    fix

--- sesion_2/2.1-Asincronismo/Ejercicio-01/index.js
+++ sesion_2/2.1-Asincronismo/Ejercicio-01/index.js
@@ -1,4 +1,3 @@
-//                  10          4
 const dividir = (dividendo, divisor, miFuncion) => {
     if (divisor === 0) {
         miFuncion({
@@ -6,7 +5,6 @@ const dividir = (dividendo, divisor, miFuncion) => {
             message: 'No se puede dividir por 0'
         })
     } else {
-//                      10      /    4
         const value = dividendo / divisor;
         miFuncion({
             error: false,
@@ -15,21 +13,10 @@ const dividir = (dividendo, divisor, miFuncion) => {
     }
 }
 
-dividir(
-    10, //dividendo
-    0,  //divisor
-    (response) => {
+dividir( 10, (response) => {
     if (response.error) {
         console.log('Ha ocurrido un error. ', response.message)
     } else {
         console.log('Resultado obtenido correctamente y es: ', response.value)
-    } // función callback
+    }
 })
-
-// dividir(10, 0, (err, result) => {
-//     if (err) {
-//         console.log('Ha ocurrido un error', err.message)
-//     } else {
-//         console.log('Resultado obtenido correctamente y es: ', result.value)
-//     }
-// })
